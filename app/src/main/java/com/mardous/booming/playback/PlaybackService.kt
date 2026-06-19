f461dd4f07088a42b5be79ea360f35fc8c803adf
WluhWluh <52004526+WluhWluh@users.noreply.github.com>
2026-06-19 02:59:18 -0700

    Fix running stem playback sample rate clock

--- app/src/main/java/com/mardous/booming/playback/PlaybackService.kt
+++ app/src/main/java/com/mardous/booming/playback/PlaybackService.kt
@@ -1560,7 +1560,7 @@ class PlaybackService :
         val shouldPlayAfterSwitch = resumeWhenReady || playWhenReady
         val originalMediaItem = song.toMediaItem(mediaItem.mediaId)
         val isRunningCache = manifest.state == SourceSeparationCacheState.Running
-        val useOriginalClock = isRunningCache || manifest.canUseOriginalSourceSeparationClock(output)
+        val useOriginalClock = manifest.canUseOriginalSourceSeparationClock(output)
         val queueReplacementToken = if (useOriginalClock) {
             null
         } else {
